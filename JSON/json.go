0b9934c6cef3a4494ba05cc67533545d1d8a9530
Ömer Faruk Fidan <fidanomerfaruk01@gmail.com>
2023-01-29 19:16:54 +0300

    Read&write JSON

--- JSON/json.go
+++ JSON/json.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 )
 
 type Person struct {
-	FirstName  string `json:"first_name"`
-	SecondName string `json:"second_name"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 }
 
 func main() {
-
+	//Unmarshalling json to struct and printing
 	myJson := `
 [
 	{
 		"first_name": "Omer",
-		"second_name": "Fidan"
+		"last_name": "Faruk"
 	}
 ]`
 	var unmarshalled []Person
@@ -28,4 +29,24 @@ func main() {
 
 	log.Printf("unmasrshalled: %v", unmarshalled)
 
+	//Write json from struct
+	var mySlice []Person
+
+	var m1 Person
+	m1.FirstName = "Ali"
+	m1.LastName = "Hakan"
+
+	mySlice = append(mySlice, m1)
+
+	var m2 Person
+	m2.FirstName = "Murat"
+	m2.LastName = "Demir"
+
+	mySlice = append(mySlice, m2)
+
+	newJson, err := json.MarshalIndent(mySlice, "", "  ")
+	if err != nil {
+		log.Println("Error while marshalling process!", err)
+	}
+	fmt.Println(string(newJson))
 }
